4ac16ce88b85eb79e9e72ea8fea608b6c6b37eea
Alex Ratner <ajratner@gmail.com>
2013-02-26 23:11:27 -0500

    Further additions to perceptron

--- perceptron.py
+++ perceptron.py
@@ -13,65 +13,93 @@ import numpy as np
 class Perceptron:
 
   def __init__(self):
-    self.W = np.array([])
-    self.W_map = []
+    self.W = []
+    self.token_maps = {}
     self.in_count = 0
     self.true_count = 0
-    self.token_maps = {}
   
   # for handling feature vectors of form e.g. [1.3, 2, ('a', 'b', 'e'), 4]
   def _handle_features_in(self, mixed_features):
-    
-    # initialization conditions
-    if len(self.W) == 0:
-      self.W = np.array([0.0 for f in mixed_features])
-      self.W_map = range(len(mixed_features))
-
-    # map features in & w to new forms
-    w = []
     x = []
-    j = 0
     for i, f in enumerate(mixed_features):
-      
-      # handle int / float features
       if type(f) == float or type(f) == int:
         x.append(f)
-        w.append(self.W[self.W_map[i]])
 
-        # update mapping for i
-        self.W_map[i] = j
-        j += 1
+        # handle initial conditions
+        if len(self.W) <= i:
+          self.W.append(0.0)
       
-      # handle token array features
-      elif type(f) == tuple or type(f) == tuple:
+      elif type(f) == list or type(f) == tuple:
         
-        # convert tokens to vector form & handle new tokens
+        # map the tokens to vector form via the token maps
         if not self.token_maps.has_key(i):
           self.token_maps[i] = {}
-        else:
-          x += [0.0 for y in range(len(self.token_maps[i]))]
-          w += [self.W[self.W_map[i]+k] for k in range(len(self.token_maps[i]))]
-        
-        # add the tokens in the feature, seen or unseen as of t
+        x.append([0.0 for j in self.token_maps[i]])
         for token in f:
           if self.token_maps[i].has_key(token):
-            x[j+self.token_maps[i][token]] = 1.0
+            x[i][self.token_maps[i][token]] = 1.0
           else:
-            x.append(1.0)
-            w.append(0.0)
+            x[i].append(1.0)
             self.token_maps[i][token] = len(self.token_maps[i])
 
-        # update mapping for i
-        self.W_map[i] = j
-        j += len(self.token_maps[i])
+        # append whole new row to W if initial conditions
+        if len(self.W) <= i:
+          self.W.append([0.0 for j in f])
 
-    # return x, update W
-    self.W = np.array(w)
-    return np.array(x)
+        # else append existing row accordingly
+        else:
+          self.W[i] += [0.0 for j in range(len(self.token_maps[i]) - len(self.W[i]))]
 
-  # primary add routine
-  def add(self, mixed_features):
-    x = self._handle_features_in(mixed_features)
-          
+    # return x np array, flattened and normalized
+    x = self._flatten(x)
+    return x/np.linalg.norm(x)
+
+  
+  # subfunction for flattening mixed array
+  def _flatten(self, mixed_array):
+    array_out = []
+    for x in mixed_array:
+      if type(x) == list:
+        array_out += x
+      elif type(x) == tuple:
+        array_out += list(x)
+      else:
+        array_out.append(x)
+    return np.array(array_out)
 
 
+  # subfuntion for un-flattening mixed array according to prototype
+  def _unflatten(self, flat_array, proto_array):
+    array_out = []
+    i = 0
+    for r in proto_array:
+      if type(r) == list or type(r) == tuple:
+        array_out.append([flat_array[i+j] for j in range(len(r))])
+        i += len(r)
+      else:
+        array_out.append(flat_array[i])
+        i += 1
+    return array_out
+
+
+  # primary routine for adding a datum and getting predicted classification back
+  def add_and_classify(self, mixed_features):
+    
+    # log that an input datum was entered
+    self.in_count += 1
+
+    # calculate the input doc's score
+    x = self._flatten(self._handle_features_in(mixed_features))
+    w = self._flatten(self.W)
+    score = np.dot(x, w)
+
+    # NOTE: !!! --> switch it to [0,1] unary rather than [-1,1], i.e. tally up based on
+    # positives only, don't get flooded with any negative indicators
+
+    # NOTE: count up the occurences of OOVs (new vocab words)- this should effect the
+    #       reporting thresh
+
+    # use dynamic threshold to classify & decide whether to wait for input
+
+    
+    
