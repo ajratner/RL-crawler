e582ea7af70ff5f530231ad32e25eb07bf79334e
Alex Ratner <ajratner@gmail.com>
2013-03-13 17:08:43 -0400

    node localc- file added

---
+++ node_locals.py
@@ -0,0 +1,9 @@
+# individual node-specific params
+
+SEED_LIST = ['http://www.onecle.com/']
+
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.57 Safari/537.17"
+
+DB_VARS = ('localhost', 'root', 'penguin25', 'crawler_test') # 'localhost' for node w/ the db
+
+NODE_START_DELAY = 0  # in seconds; allows node to receive messages but does not start actual crawl
